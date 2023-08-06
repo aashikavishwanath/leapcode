5f3443e2a77f1cf2b43ea037f7df70954a1ce8d8
Aashika Vishwanath <aashika@aashika.com>
2023-08-06 08:52:48 +0000

    -edits

--- src/components/Models/Login.tsx
+++ src/components/Models/Login.tsx
@@ -1,6 +1,9 @@
 import { authModelState } from '@/atoms/authModelAtom';
-import React from 'react';
 import { useSetRecoilState } from 'recoil';
+import { auth } from "@/firebase/firebase";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
 
 type LoginProps = {
     
@@ -11,14 +14,36 @@ const Login:React.FC<LoginProps> = () => {
     const handleClick = (type: "login" | "register" | "forgotPassword") => {
 		setAuthModelState((prev) => ({ ...prev, type }));
 	};
+    const [inputs, setInputs] = useState({ email: "", password: "" });
+	const [signInWithEmailAndPassword, user, loading, error] = useSignInWithEmailAndPassword(auth);
+	const router = useRouter();
+	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+	};
+
+    const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		if (!inputs.email || !inputs.password) return alert("Please fill all fields");
+		try {
+			const newUser = await signInWithEmailAndPassword(inputs.email, inputs.password);
+			if (!newUser) return;
+			router.push("/");
+		} catch (error: any) {
+			alert(error.message);
+		}
+	};
+
+	useEffect(() => {
+		if (error) alert(error.message);
+	}, [error]);
     return (
-    <form className="space-y-6 px-6 py-3 pb-8 pt-0">
+    <form className="space-y-6 px-6 py-3 pb-8 pt-0" onSubmit={handleLogin}>
         <h3 className="text-xl font-mono text-gray-800">Sign in to LeapCode</h3>
         <div>
             <label htmlFor='email' className='text-sm font-mono block mb-2 text-gray-600'>
                 Your Email:
             </label>
-            <input type='email' name='email' id='email' 
+            <input onChange={handleInputChange} type='email' name='email' id='email' 
             className='border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-700 focus:border-blue-700 block w-full p-2.5
             bg-gray-200 border-gray-500 placeholder-gray-400 text-gray-800 font-mono'
             placeholder='name@company.com'
@@ -28,7 +53,7 @@ const Login:React.FC<LoginProps> = () => {
             <label htmlFor='password' className='text-sm font-mono block mb-2 text-gray-600'>
                 Your Password:
             </label>
-            <input type='password' name='password' id='password' 
+            <input onChange={handleInputChange} type='password' name='password' id='password' 
             className='border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-700 focus:border-blue-700 block w-full p-2.5
             bg-gray-200 border-gray-500 placeholder-gray-400 text-gray-800 font-mono'
             placeholder='******'
@@ -36,7 +61,7 @@ const Login:React.FC<LoginProps> = () => {
         </div>
         <button type='submit' className='w-full text-white focus:ring-blue-300 font-mono rounded-lg
                 text-lg px-5 py-3 text-center bg-red-500 hover:bg-red-700'>
-        Login
+        {loading ? "Loading..." : "Log In"}
         </button>
         <button className='flex w-full justify-end font-mono' onClick={() => handleClick("forgotPassword")}>
 				<a href='#' className='text-sm block text-blue-700 hover:underline  w-full text-right'>
--- src/components/Models/Signup.tsx
+++ src/components/Models/Signup.tsx
@@ -1,6 +1,11 @@
 import { authModelState } from '@/atoms/authModelAtom';
 import React from 'react';
 import { useSetRecoilState } from 'recoil';
+import { useEffect, useState } from "react";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { auth } from "@/firebase/firebase";
+import { useRouter } from "next/router";
+
 
 type SignupProps = {
     
@@ -11,14 +16,43 @@ const Signup:React.FC<SignupProps> = () => {
     const handleClick = () => {
 		setAuthModelState((prev) => ({ ...prev, type: "login" }));
 	};
+    const router = useRouter();
+    const [inputs, setInputs] = useState({ email: "", userName: "", password: "" });
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useCreateUserWithEmailAndPassword(auth);
+    const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+	};
+
+    const handleRegister = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+        if (!inputs.email || !inputs.password || !inputs.userName) return alert("Please fill all fields");
+        try {
+			const newUser = await createUserWithEmailAndPassword(inputs.email, inputs.password);
+			if (!newUser) return;
+			router.push("/");
+		} catch (error: any) {
+			alert(error.message);
+        }
+	};
+
+    useEffect(() => {
+		if (error) alert(error.message);
+	}, [error]);
+
     return (
-    <form className="space-y-6 px-6 py-3 pb-8 pt-0">
+    <form className="space-y-6 px-6 py-3 pb-8 pt-0" onSubmit={handleRegister}>
         <h3 className="text-xl font-mono text-gray-800">Register to LeapCode</h3>
         <div>
             <label htmlFor='email' className='text-sm font-mono block mb-2 text-gray-600'>
                 Email:
             </label>
-            <input type='email' name='email' id='email' 
+            
+            <input onChange={handleChangeInput} type='email' name='email' id='email' 
             className='border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-700 focus:border-blue-700 block w-full p-2.5
             bg-gray-200 border-gray-500 placeholder-gray-400 text-gray-800 font-mono'
             placeholder='name@company.com'
@@ -28,7 +62,7 @@ const Signup:React.FC<SignupProps> = () => {
             <label htmlFor='userName' className='text-sm font-mono block mb-2 text-gray-600'>
                 Username:
             </label>
-            <input type='userName' name='userName' id='userName' 
+            <input onChange={handleChangeInput} type='userName' name='userName' id='userName' 
             className='border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-700 focus:border-blue-700 block w-full p-2.5
             bg-gray-200 border-gray-500 placeholder-gray-400 text-gray-800 font-mono'
             placeholder='firstname.lastname'
@@ -38,7 +72,7 @@ const Signup:React.FC<SignupProps> = () => {
             <label htmlFor='password' className='text-sm font-mono block mb-2 text-gray-600'>
                 Password:
             </label>
-            <input type='password' name='password' id='password' 
+            <input onChange={handleChangeInput} type='password' name='password' id='password' 
             className='border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-700 focus:border-blue-700 block w-full p-2.5
             bg-gray-200 border-gray-500 placeholder-gray-400 text-gray-800 font-mono'
             placeholder='******'
@@ -46,7 +80,7 @@ const Signup:React.FC<SignupProps> = () => {
         </div>
         <button type='submit' className='w-full text-white focus:ring-blue-300 font-mono rounded-lg
                 text-lg px-5 py-3 text-center bg-red-500 hover:bg-red-700'>
-        Register
+        {loading ? "Registering..." : "Register"}
         </button>
 			<div className='text-sm font-mono text-red'>
 				Already have an account?{" "}
